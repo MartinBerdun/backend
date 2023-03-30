a00827d5d2921c65bb8ecedbf1dd7923bb8cade4
Martin Berdun <mberdun.98@gmail.com>
2023-03-29 21:06:26 -0300

    Websockets corregido

--- js/src/public/js/realTimeProducts.js
+++ js/src/public/js/realTimeProducts.js
@@ -13,6 +13,25 @@ deleteProduct.addEventListener('submit', (e) => {
     })
 });
 
+addProduct.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const prod = {
+        title: title.value,
+        description: description.value,
+        code: code.value,
+        price: price.value,
+        stock: stock.value,
+        category: category.value
+    }
+    fetch(`/api/products/`, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(prod)
+    })
+})
 
 //recibe un mensaje del servidor
 socket.on("products", (products) => {
