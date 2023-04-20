9d51db38e0cc34096904f305c146bcf780007a90
Martin Berdun <mberdun.98@gmail.com>
2023-04-20 20:41:42 -0300

    In progress

--- js/src/public/js/products.js
+++ js/src/public/js/products.js
@@ -1,22 +1,28 @@
 const addToCart = document.querySelectorAll('[id^="addToCart-"]');
-// const productId = form.getAttribute("id").split("-")[1];
-// console.log(productId);
+const productId = document.getElementById("this_.id");
 
-// const addToCart = document.getElementById("addToCart")
+
 
 addToCart.forEach((form) => {
     form.addEventListener("submit", (e) => {
         e.preventDefault();
-        // const productId = form.getAttribute("id").split("-")[1];
-        // const prodTitle = form.closest("div").querySelector("h5").textContent;
+
+        const quantity = {
+            "quantity":3,
+        }
     
         fetch(`/api/carts/64400c635ab188f813fab0f7/product/${productId}`, {
             method: "POST",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(quantity)
         })
             .then(() => {
             Swal.fire({
                 title: "Product added to cart!",
-                text: `You added ${prodTitle} to Cart`,
+                text: `You added one product to Cart`,
                 toast: true,
                 position: "top-right",
                 icon: "success",
