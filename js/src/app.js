856c6809f3dab448a187a429384e716a85d56429
MartinBerdun <mberdun.98@gmail.com>
2023-07-17 22:56:16 -0300

    Swagger en products

--- js/src/app.js
+++ js/src/app.js
@@ -6,12 +6,8 @@ import __dirname from "./utils.js"; //representa la ruta absoluta de los archivo
 import viewsRouter from "./routes/views.router.js";
 import socket from "./socket.js";
 import cookieParser from "cookie-parser";
-// import mongoose from 'mongoose';
-// import session from 'express-session';
-// import MongoStore from 'connect-mongo';
 import database from "./db.js";
 import morgan from "morgan";
-// import config from './config.js';
 import userRouter from "./routes/user.router.js";
 import passport from "passport";
 import initializePassport from "./auth/passport.js";
@@ -22,24 +18,16 @@ import { errorMiddleware } from "./middlewares/error.js";
 import { addLogger } from "./utils/logger.js";
 import loggerRouter from "./routes/logger.router.js"
 
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUiExpress from 'swagger-ui-express';
+
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(`${__dirname}/public`));
-/* app.use(morgan("dev"));
- */ app.use(cookieParser());
-//     session({
-//         store: MongoStore.create({
-//             mongoUrl: config.dbUrl,
-//             ttl: 120,
-//         }),
+app.use(cookieParser());
 
-//     secret:"config.SESSION_SECRET",
-//     resave:true,
-//     saveUninitialized:false,
-// })
-// )
 
 //Passport
 initializePassport();
@@ -47,6 +35,22 @@ app.use(passport.initialize());
 // app.use(passport.session());
 
 app.use(addLogger);
+
+const swaggerOPtions = {
+  definition :{
+      openapi : "3.0.1" ,//hace referencia a la version del estandar de la documentacion a usar
+      info: {
+          title: "Adopt Me API",
+          description: "Documentacíon que soporta al sistema Adopt Me",
+      },
+  },
+  apis: [`${__dirname}/docs/**/*.yaml`],
+}
+
+const spec = swaggerJSDoc(swaggerOPtions)
+
+app.use("/apidocs", swaggerUiExpress.serve, swaggerUiExpress.setup(spec))
+
 
 
 //Routes
@@ -58,6 +62,7 @@ app.use("/api/logger", loggerRouter);
 app.use("/", viewsRouter);
 
 app.use(errorMiddleware);
+
 //configuracion de handlebars
 app.engine("handlebars", handelbars.engine()); //crea un motor de plantillas llamado handlebars
 app.set("views", `${__dirname}/views`); //le dice al servidor donde encuentra las plantillas
--- js/src/routes/product.router.js
+++ js/src/routes/product.router.js
@@ -6,9 +6,13 @@ import { authRole } from "../middlewares/auth.js";
 const router = Router();
 
 router.get ("/", getProducts);
+
 router.get ("/:pid", getProductById);
+
 router.post ("/",(req, res, next) => authRole(req, res, next, ["admin", "premium"]), addProduct);
+
 router.put ("/:pid",(req, res, next) => authRole(req, res, next, "admin"), updateProduct);
+
 router.delete ("/:pid",(req, res, next) => authRole(req, res, next, ["admin", "premium"]),deleteProduct);
 
 router.post("/mockingproducts", mockingProducts);
